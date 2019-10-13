fea299bab77aa6bc357f4df1e8eabb73f3faff6a
Céline Ly <celinely1999@gmail.com>
2019-10-13 09:12:32 -0400

    fix expanded sheet for output panel (kind of), added changeInputs(), added navigation highlight

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var compressed = false;
+var inputVisible = false;
+var outputClick = false;
 
 window.onload = initiate;
 
@@ -24,6 +26,7 @@ function initiateNavigationBar(){
 
 }
 function setOutputLinks(){
+	var homeBtn = $("#homeBtn");
 	var graphBtn = $("#graphBtn");
 	var customerBtn = $("#costumerBtn");
 	var volumeBtn = $("#volumeBtn");
@@ -33,17 +36,33 @@ function setOutputLinks(){
 	var volumeWrapper = $("#volumeWrapper");
 
 	// set the onclick function of the buttons
+	homeBtn.click(function(){
+		outputClick = false;
+		$("#outputBtn > #icon").css("color","gray");
+		$("#homeBtn > #icon").css("color","red");
+		graphDiv.css("display","none");
+		volumeWrapper.css("display","none");
+		customersDiv.css("display","none");
+		
+	});
 
 	graphBtn.click(function(){
+		outputClick = true;
 		showDiv(graphDiv,customersDiv,volumeWrapper);
+		$("#homeBtn > #icon").css("color","gray");
+		
 	});
 
 	customerBtn.click(function(){
+		outputClick = true;
 		showDiv(customersDiv,graphDiv,volumeWrapper);
+		$("#homeBtn > #icon").css("color","gray");
 	});
 
 	volumeBtn.click(function(){
+		outputClick = true;
 		showDiv(volumeWrapper,graphDiv,customersDiv);
+		$("#homeBtn > #icon").css("color","gray");
 
 
 	});
@@ -65,8 +84,10 @@ function setInputPanel(){
 
 function setInputNavButton(){
 	$("#inputBtn").click(function(){
+		inputVisible = true;
 		scrollToTop();
 		$("#inputPanel").slideDown("slow");
+		$("#inputBtn > #icon").css("color","red");
 		$("#outputPanel").css("margin-top", "10px");
 		$("#volumeBody").css("margin-right", "-50px");
 		$("#volumeBody").css("padding-left", "5%");
@@ -82,7 +103,7 @@ function setHoverEffect(element){
 	$(element).hover(function(){
 		var fontSize = "21px";
 		if(compressed)
-			fontSize = "11px"
+			fontSize = "11px";
 		$("#outputBtn").css("font-size",fontSize);
 		if(!compressed){
 			$("#outputBtn").css("border-style","solid");
@@ -100,9 +121,9 @@ function setHoverEffect(element){
 		if(compressed)
 			fontSize = "10px"
 		$("#outputBtn").css("font-size",fontSize);
-				$("#outputBtn").css("border-style","none");
-
-		$("#outputBtn > #icon").css("color","gray");
+		$("#outputBtn").css("border-style","none");
+		if(!outputClick)
+			$("#outputBtn > #icon").css("color","gray");
 		$("#outputBtn > #icon").css("font-size","24px");
 		$("#outputBtn > #icon").css("left","30px");
 
@@ -111,8 +132,9 @@ function setHoverEffect(element){
 function setInputCloseButton(){
 
 	$("#inputCloseButton").click(function(){
+		inputVisible = false;
 		$("#inputPanel").slideUp("slow");
-		
+		$("#inputBtn > #icon").css("color","gray");
 		$("#outputPanel").css("margin-top", "60px");
 		$("#volumeBody").css("margin-right", "-60px");
 		$("#volumeBody").css("padding-left", "15%");
@@ -131,6 +153,21 @@ function setCompressedSheet(){
 	
 	swapNavigationStyleSheet("navBarStyleCompressed.css");
 
+	//Recentre le customerBody et le volumeBody lorsque le menu est compressé
+	if(inputVisible){
+		$("#costumerBody").css("padding-right","10%");
+		$("#volumeBody").css("padding-right","10%");
+	}else{
+		$("#costumerBody").css("padding-right","25%");
+		$("#volumeBody").css("padding-right","20%");
+	}
+	$("#costumerBody").css("width","60%");
+	$("#costumerBody img").css("padding-left", "15%");
+	$("#volumeBody").css("width","60%");
+	$("#volumeBody img").css("padding-left", "15%");
+	$("#graphWrapper").css("width","52%");
+	$("#graphWrapper").css("padding-left","20%");
+
 	setHoverEffect("#outputBtn");
 	$("#outputBtn").css("font-size","10px");
 	
@@ -148,6 +185,16 @@ function setExpandedSheet(){
 	
 	swapNavigationStyleSheet("navBarStyleExpanded.css");
 
+	//Recentre le customerBody
+
+	$("#costumerBody").css("width","70%");
+	$("#costumerBody").css("padding-right","1%");
+	$("#costumerBody img").css("padding-left", "0%");
+	$("#volumeBody").css("width","70%");
+	$("#volumeBody").css("padding-right","5%");
+	$("#volumeBody img").css("padding-left", "0");
+	$("#graphWrapper").css("width","60%");
+	$("#graphWrapper").css("padding-left","15%");
 
 	setHoverEffect("#outputBtn");
 	$("#outputBtn").css("font-size","20px");
@@ -164,3 +211,15 @@ function swapNavigationStyleSheet(sheet){
 function scrollToTop(){
 	window.scrollTo({top:0,behavior:'smooth'});
 }
+
+// Inputs changes
+changeInputs = function () {
+	var imgCS = document.getElementById("imgCS");
+	var imgVL = document.getElementById("imgVL");
+	var imgGraph = document.getElementById("imgGraph");
+  
+	imgCS.src = "img/recontractConsumerSegmentMAJ.png";
+	imgVL.src = "img/newCustomerConsumerSegmentMAJ.png";
+	imgGraph.src = "img/graphMAJ.png";
+  
+  }
